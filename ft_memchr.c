51ea946ba5cfb0c6b748a5fbc152f871e3026985
Luiz-Pastor <luigpacr5@gmail.com>
2023-09-15 22:45:47 +0200

    Fixed bug and added 42 header

--- ft_memchr.c
+++ ft_memchr.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lpastor- <lpastor-@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/12 09:23:17 by lpastor-          #+#    #+#             */
+/*   Updated: 2023/09/12 09:23:17 by lpastor-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 void	*ft_memchr(const void *str, int c, size_t n)
@@ -9,7 +21,7 @@ void	*ft_memchr(const void *str, int c, size_t n)
 	str_casted = (char *)str;
 	while (index < n)
 	{
-		if (str_casted[index] == (unsigned char)c)
+		if ((unsigned char)str_casted[index] == (unsigned char)c)
 			return ((void *)&str_casted[index]);
 		index++;
 	}
