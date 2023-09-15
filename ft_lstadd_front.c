4cb3422972c3449bafc9793f0aa73b4997604dff
Luiz-Pastor <luigpacr5@gmail.com>
2023-09-15 22:56:03 +0200

    Fixed error and added 42 header

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -1,8 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_front.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lpastor- <lpastor-@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/12 09:22:38 by lpastor-          #+#    #+#             */
+/*   Updated: 2023/09/12 09:22:38 by lpastor-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-	if (!lst || !*lst || !new)
+	if (!lst || !new)
 		return ;
 	new->next = *lst;
 	*lst = new;
