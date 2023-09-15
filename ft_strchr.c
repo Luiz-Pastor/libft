a23c6134411a0036ce354fa0fe9a9f1f58a6e54d
Luiz-Pastor <luigpacr5@gmail.com>
2023-09-15 22:38:30 +0200

    Fixed bugs and added 42 header

--- ft_strchr.c
+++ ft_strchr.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lpastor- <lpastor-@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/12 09:24:08 by lpastor-          #+#    #+#             */
+/*   Updated: 2023/09/12 09:24:08 by lpastor-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 char	*ft_strchr(const char *str, int c)
@@ -11,5 +23,7 @@ char	*ft_strchr(const char *str, int c)
 			return ((char *)&str[index]);
 		index++;
 	}
+	if (!str[index] && !((char)c))
+		return ((char *)&str[index]);
 	return (NULL);
 }
