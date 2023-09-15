d6e6258bff16d652743140b715df3119660c6103
Luiz-Pastor <luigpacr5@gmail.com>
2023-09-15 19:11:37 +0200

    ft_strnstr updated

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,30 +1,28 @@
 #include "libft.h"
+#include <string.h>
+#include <stdio.h>
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
-	size_t	index_big;
-	size_t	index_lit;
-	size_t	index_temp;
+	size_t	i;
+	size_t	j;
 
-	index_big = 0;
-	if (!little || !little[0])
+	i = 0;
+	if (!little[0])
 		return ((char *)big);
-	while (index_big < len && big[index_big])
+	while (big[i] && i < len)
 	{
-		if (big[index_big] == little[0])
+		if (big[i] == little[0])
 		{
-			index_lit = 0;
-			index_temp = index_big;
-			while (big[index_big] == little[index_lit] && index_big < len)
+			j = 0;
+			while (little[j] == big[i + j] && (i + j) < len)
 			{
-				index_big++;
-				index_lit++;
+				if (little[j + 1] == '\0')
+					return ((char *)&big[i]);
+				j++;
 			}
-			if (little[index_lit] == '\0')
-				return ((char *)&big[index_big - index_lit]);
-			index_big = index_temp;
 		}
-		index_big++;
+		i++;
 	}
 	return (NULL);
 }
