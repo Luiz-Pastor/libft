8bc9bb5879d96249d6833042db4223ef5765d632
Luiz-Pastor <luigpacr5@gmail.com>
2023-09-15 23:15:44 +0200

    Added header and updated

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -1,28 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lpastor- <lpastor-@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/12 09:24:35 by lpastor-          #+#    #+#             */
+/*   Updated: 2023/09/12 09:24:35 by lpastor-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	size_t	index;
-	size_t	src_length;
+	size_t	length;
 
 	index = 0;
-	src_length = ft_strlen(src);
-	if (!src)
-		return (0);
-	if ((long long) size < 0)
-		size = 1 + src_length;
-	if (src_length > 0 && size > 1)
+	length = ft_strlen(src);
+	if (size == 0)
+		return (length);
+	while (src[index] && index < (size - 1))
 	{
-		while (index < (size - 1))
-		{
-			if (index < src_length)
-				dst[index] = src[index];
-			else if (index == src_length)
-				dst[index] = '\0';
-			index++;
-		}
+		dst[index] = src[index];
+		index++;
 	}
-	if (size > 0)
-		dst[index] = '\0';
-	return (src_length);
+	dst[index] = '\0';
+	return (length);
 }
